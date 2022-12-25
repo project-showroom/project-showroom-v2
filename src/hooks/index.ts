9e35c1491cea48b104f916e61ed024d95ba6a05f
emremutlu08 <mutluemre93@gmail.com>
2022-12-25 17:55:44 +0300

    New: Hooks Folder Usage Example Is Added!

---
+++ src/hooks/index.ts
@@ -0,0 +1,11 @@
+import { useEventCallback } from "./useEventCallback";
+import { useEventListener } from "./useEventListener";
+import { useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
+import { useLocalStorage } from "./useLocalStorage";
+
+export {
+  useLocalStorage,
+  useEventCallback,
+  useIsomorphicLayoutEffect,
+  useEventListener,
+};
