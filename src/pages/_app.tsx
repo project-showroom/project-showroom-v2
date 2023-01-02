30eb9020e8728a48e3f6b3e6254c139f906f1927
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-02 12:00:07 +0300

    Refactor: Icon&Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import classNames from 'classnames';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 
 import { HeaderFeature } from '../features/header-feature/header-feature';
@@ -12,15 +13,17 @@ export default function App({ Component, pageProps }: AppProps) {
     'flex justify-between items-center pl-2 py-3 font-bold bg-blue-600 text-white shadow-lg shadow-gray-300/50',
   );
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Provider store={store}>
-        <main>
-          <header className={headerClassNames}>
-            <HeaderFeature />
-          </header>
-          <Component {...pageProps} />
-        </main>
-      </Provider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Provider store={store}>
+          <main>
+            <header className={headerClassNames}>
+              <HeaderFeature />
+            </header>
+            <Component {...pageProps} />
+          </main>
+        </Provider>
+      </ThemeProvider>
+    </>
   );
 }
