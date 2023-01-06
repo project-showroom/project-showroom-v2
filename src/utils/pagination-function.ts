863b9bd9f65f0fcbf75505928554c0363a40f161
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-06 14:05:55 +0300

    Refactor: card-pagination

--- src/utils/pagination-function.ts
+++ src/utils/pagination-function.ts
@@ -1,9 +1,14 @@
-import { IPaginationFunctionType } from '../types/utils-types/pagination-function-type';
+// import { IPaginationFunctionType } from '../types/utils-types/pagination-function-type';
 
-export const paginationFunction = ({
-  array,
-  page_size,
-  page_number,
-}: IPaginationFunctionType) => {
-  return array.slice((page_number - 1) * page_size, page_number * page_size);
+export const paginationFunction = (
+  array: any,
+  page_number: any,
+  ITEMS_PER_PAGE: any,
+) => {
+  const pageArray = array.slice(
+    (page_number - 1) * ITEMS_PER_PAGE,
+    page_number * ITEMS_PER_PAGE,
+  );
+
+  return { pageArray };
 };
