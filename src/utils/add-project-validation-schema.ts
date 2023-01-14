4c0cc4f7c655eeb488efa30e77538b10c8cd63e9
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-14 12:00:38 +0300

    Refactor: add-project-validation

---
+++ src/utils/add-project-validation-schema.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+  projectTitle: yup.string().required('Required'),
+  thumbnailUrl: yup.string().required('Required'),
+  leftButtonUrl: yup.string().required('Required'),
+  rightButtonUrl: yup.string().required('Required'),
+});
+
+export default validationSchema;
