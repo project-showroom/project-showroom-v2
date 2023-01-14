f76198b8085499b0d627875c9cc098c713e6accc
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-14 11:59:11 +0300

    Refactor: database

--- src/libs/database.ts
+++ src/libs/database.ts
@@ -3,22 +3,9 @@ import mongoose from 'mongoose';
 const MONGODB_URI: any = process.env.MONGODB_URI;
 
 async function connect() {
-  const opts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    bufferCommands: false,
-  };
-  mongoose
-    .connect(MONGODB_URI, opts)
-    .then(() => {
-      /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-    })
-    .catch((err) => {
-      console.log(
-        `MongoDB connection error. Please make sure MongoDB is running. ${err}`,
-      );
-      // process.exit();
-    });
+  mongoose.connect(MONGODB_URI).then((mongoose) => {
+    return mongoose;
+  });
 }
 
 export default connect;
