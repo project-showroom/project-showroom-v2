96eb953b247e18f8d955726a574925bb3ce156fc
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-17 21:54:09 +0300

    Refactor: edit-project-page

---
+++ src/pages/edit-project/index.tsx
@@ -0,0 +1,23 @@
+import { useId } from 'react';
+
+import Box from '@mui/material/Box';
+import classNames from 'classnames';
+
+import { EditProjectFeature } from '../../features/edit-project-feature/edit-project-feature';
+
+const COMPONENT_NAME = 'EditProjectPage';
+export default function EditProjectPage(props: { className?: string }) {
+  const id = useId() + '-' + COMPONENT_NAME;
+  const { ...rest } = props;
+
+  const mainClassNames = classNames(
+    'flex relative flex-col h-screen p-4 top-16',
+  );
+  return (
+    <div id={id} {...rest} className={mainClassNames}>
+      <Box>
+        <EditProjectFeature />
+      </Box>
+    </div>
+  );
+}
