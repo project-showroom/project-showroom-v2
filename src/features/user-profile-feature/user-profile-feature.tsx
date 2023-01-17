dc9373022f4c8c75a67e55368bd13d51d71e5060
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-17 15:41:44 +0300

    Refactor:use-get-projects

--- src/features/user-profile-feature/user-profile-feature.tsx
+++ src/features/user-profile-feature/user-profile-feature.tsx
@@ -8,10 +8,10 @@ import { UserProfileButtons } from './components/user-profile-buttons';
 import { CardFeature } from '../card-feature';
 
 const COMPONENT_NAME = 'UserProfileFeature';
-const UserProfileFeature = (props: { className?: string }) => {
+const UserProfileFeature = (props: { className?: string; projects: any }) => {
   const id = useId() + '-' + COMPONENT_NAME;
 
-  const { className, ...rest } = props;
+  const { className, projects, ...rest } = props;
 
   return (
     <div id={id} {...rest} className={classNames(className, COMPONENT_NAME)}>
@@ -21,7 +21,7 @@ const UserProfileFeature = (props: { className?: string }) => {
           <UserProfileDescription />
           <UserProfileButtons />
           <Divider />
-          <CardFeature />
+          <CardFeature projects={projects} />
         </Container>
       </>
     </div>
