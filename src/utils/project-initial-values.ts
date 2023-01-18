7ee0db02fb761c110e1e81023e585ca4f7667022
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-18 14:02:42 +0300

    Refactor: initial-value-form

--- src/utils/project-initial-values.ts
+++ src/utils/project-initial-values.ts
@@ -1,19 +1,31 @@
 import { IAddProjectFormValues } from '../types/element-types/form-elements-types';
 
-const initialValues: IAddProjectFormValues = {
-  projectTitle: '',
-  thumbnailUrl: '',
-  description: '',
-  skillTags: [],
-  leftButtonTitle: 'View Online',
-  leftButtonUrl: '',
-  rightButtonTitle: 'View Codes',
-  rightButtonUrl: '',
-  userInfo: {
-    userId: '',
-    defaultUserName: '',
-    displayName: '',
-  },
+const initialValuesFunc = (
+  editProject: IAddProjectFormValues | any,
+  tags: any,
+) => {
+  if (editProject && tags)
+    return {
+      projectTitle: editProject.projectTitle,
+      thumbnailUrl: editProject.thumbnailUrl,
+      description: editProject.description,
+      skillTags: tags,
+      leftButtonTitle: 'View Online',
+      leftButtonUrl: editProject.leftButtonUrl,
+      rightButtonTitle: 'View Codes',
+      rightButtonUrl: editProject.rightButtonUrl,
+      userInfo: { ...editProject.userInfo },
+    };
+  return {
+    projectTitle: '',
+    thumbnailUrl: '',
+    description: '',
+    skillTags: tags,
+    leftButtonTitle: 'View Online',
+    leftButtonUrl: '',
+    rightButtonTitle: 'View Codes',
+    rightButtonUrl: '',
+    userInfo: '',
+  };
 };
-
-export default initialValues;
+export default initialValuesFunc;
