e30b7f543ce236d1140e5877e7f5f814481988b2
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-18 14:21:42 +0300

    Docs: change-location

---
+++ src/pages/[username]/index.tsx
@@ -0,0 +1,41 @@
+import { useId, useEffect } from 'react';
+
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { UserProfileFeature } from '../../features/user-profile-feature';
+import { allProjects } from '../../store/take-projects-slice';
+
+const COMPONENT_NAME = 'UserPage';
+const UserPage = (props: { className?: string }) => {
+  const id = useId() + '-' + COMPONENT_NAME;
+  const { ...rest } = props;
+
+  const router = useRouter();
+  const { username } = router.query;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!username) return;
+    dispatch(allProjects(username) as any);
+  }, [dispatch, username]);
+
+  const { projects, loading } = useSelector((state: any) => state.projects);
+
+  if (loading && username) return <div>Loading...</div>;
+
+  const userClassNames = classNames(
+    'flow-root relative min-[320px]-justify-center top-16',
+  );
+  return (
+    <div id={id} {...rest} className={userClassNames}>
+      {username && <UserProfileFeature projects={projects} />}
+    </div>
+  );
+};
+
+UserPage.displayName = COMPONENT_NAME;
+
+export default UserPage;
