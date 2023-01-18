3e25a3d5e8e698f534cc5533a58c8aa0683e4165
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-18 14:11:07 +0300

    Refactor: take-project-for-edit

---
+++ src/store/edit-project-slice.ts
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+
+const token = getCookie('token');
+
+export const getCardProject = createAsyncThunk(
+  'projects/edit-project/[cardid]',
+  async (cardid: any) => {
+    if (!token) return;
+    return await axios
+      .get(`/api/projects/edit-project/${cardid}`)
+      .then((response) => {
+        return response.data.data[0];
+      });
+  },
+);
+
+interface IInitialState {
+  loading: boolean;
+  editProject: any;
+  error: any;
+}
+const initialState: IInitialState = {
+  loading: false,
+  editProject: [],
+  error: '',
+};
+
+const editProjectSlice: any = createSlice({
+  name: 'editProject',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCardProject.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(
+      getCardProject.fulfilled,
+      (state, action: PayloadAction) => {
+        state.loading = false;
+        state.editProject = action.payload;
+        state.error = '';
+      },
+    );
+    builder.addCase(getCardProject.rejected, (state, action) => {
+      state.loading = false;
+      state.editProject = [];
+      state.error = action.error.message || 'Something went wrong';
+    });
+  },
+});
+
+export default editProjectSlice.reducer;
