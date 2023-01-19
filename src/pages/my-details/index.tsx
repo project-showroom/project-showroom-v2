d2dd2b04d8944cf174420615425ed6d71a2fd2c5
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-19 13:40:27 +0300

    Refactor: control-profile-have-or-not

--- src/pages/my-details/index.tsx
+++ src/pages/my-details/index.tsx
@@ -1,14 +1,26 @@
-import { useId } from 'react';
+import { useId, useEffect } from 'react';
 
 import { Box } from '@mui/material';
 import classNames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MyDetailsFeature } from '../../features/my-details-feature';
+import { getCurrentProfile } from '../../store/take-profile-slice';
 
 const COMPONENT_NAME = 'Index';
 export default function MyDetailsPage(props: { className?: string }) {
   const id = useId() + '-' + COMPONENT_NAME;
   const { ...rest } = props;
+
+  const dispatch = useDispatch();
+
+  const { loading } = useSelector((state: any) => state.profile);
+
+  useEffect(() => {
+    dispatch(getCurrentProfile() as any);
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
 
   const mainClassNames = classNames(
     'flex relative flex-col h-screen p-4 top-16',
