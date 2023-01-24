21fd85596056b8e0f4d08c64cb9b436d6aa8aee6
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-01-24 14:59:24 +0300

    Refactor: created-info-type-for-google-callback

--- src/types/api-types/index.ts
+++ src/types/api-types/index.ts
@@ -1,5 +1,6 @@
 import { CardSelfProps } from './card-types';
+import { IInfo } from './profil-info-types';
 import { IProfileType } from './profile-types';
 import { IUserType } from './user-types';
 
-export type { IUserType, CardSelfProps, IProfileType };
+export type { IUserType, CardSelfProps, IProfileType, IInfo };
---
+++ src/types/api-types/profil-info-types.ts
@@ -0,0 +1,4 @@
+export interface IInfo {
+  message: string;
+  token: string;
+}
