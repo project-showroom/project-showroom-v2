89ecfa71d9f4dd870946ae4109adda55f4d2cacb
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-03-08 11:58:10 +0300

    Refactor: mui-mode-color

--- src/utils/theme-mode.ts
+++ src/utils/theme-mode.ts
@@ -3,12 +3,38 @@ import { createTheme } from '@mui/material';
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    loginButtonColor: {
+      main: '#434242',
+      contrastText: '#fff',
+    },
   },
 });
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    loginButtonColor: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
   },
 });
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    loginButtonColor: Palette['primary'];
+  }
+
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    loginButtonColor?: PaletteOptions['primary'];
+  }
+}
+
+// Update the Button's color prop options
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    loginButtonColor: true;
+  }
+}
 
 export { lightTheme, darkTheme };
