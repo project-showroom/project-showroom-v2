d1c7b1dfe196975e9e54b9b7f583d7d3a81d800b
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-03-08 09:37:41 +0300

    Refactor: error-solved

--- src/libs/database.ts
+++ src/libs/database.ts
@@ -3,10 +3,21 @@ import mongoose from 'mongoose';
 const MONGODB_URI: string =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/nextjs';
 
-async function connect() {
-  mongoose.connect(MONGODB_URI).then((mongoose) => {
-    return mongoose;
-  });
-}
+// async function connect() {
+//   mongoose.connect(MONGODB_URI).then((mongoose) => {
+//     return mongoose;
+//   });
+// }
+const connect = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(MONGODB_URI, () => {
+      console.log('MongoDB Connected...');
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    process.exit(1);
+  }
+};
 
 export default connect;
