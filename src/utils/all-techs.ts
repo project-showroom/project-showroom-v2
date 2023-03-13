028db5912a2aaa3498d3612fa5ef216f83bdf738
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-03-13 09:04:11 +0300

    Refactor: tech-names-edited

--- src/utils/all-techs.ts
+++ src/utils/all-techs.ts
@@ -1,3 +1,11 @@
 import * as icons from '@icons-pack/react-simple-icons';
 
-export const allTechs = Object.keys(icons);
+const techName = Object.keys(icons);
+
+const allTechs = techName.map((tech) => {
+  const usingSplit = tech.split('');
+  const removedSi = usingSplit.splice(2, usingSplit.length).join('');
+  return removedSi;
+});
+
+export { allTechs };
