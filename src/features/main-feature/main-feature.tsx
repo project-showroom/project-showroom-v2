ab6064a0f5a50647100c6553af4d4a677aad49d6
emremutlu08 <mutluemre93@gmail.com>
2023-03-22 21:53:29 +0300

    New: Landing page is added!

--- src/features/main-feature/main-feature.tsx
+++ src/features/main-feature/main-feature.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import Link from 'next/link';
 
 import { ViewButton } from '../../components/button-components';
+import { FormTitleElement } from '../../components/form-elements';
 import { BaseTypography } from '../../components/typography-elements';
 import { IUserType } from '../../types/api-types';
 import { urlHomePage } from '../../utils/url-home-page';
@@ -14,6 +15,51 @@ const MainFeature = (props: { user: IUserType }) => {
   const { user } = props;
 
   const userUrl = urlHomePage(user?.defaultUserName)?.href;
+
+  if (!user)
+    return (
+      <div className="space-y-1 px-2">
+        <FormTitleElement className="text-center">
+          Welcome to Project-Showroom!
+        </FormTitleElement>
+        <p>
+          Project-Showroom is a web app that allows developers and designers to
+          showcase their projects in one place. Whether you are a beginner or an
+          experienced developer, Project-Showroom is the perfect platform to
+          showcase your work and share it with the world.
+        </p>
+        <b>Features:</b>
+        <ul className="text-gray-700 list-disc list-inside dark:text-gray-400">
+          <li>
+            Showcase your portfolio: Project-Showroom allows you to create a
+            portfolio of your work, including images, descriptions, and links to
+            your projects.
+          </li>
+          <li>
+            Discover other portfolios: Explore other developers&apos; and
+            designers&apos; portfolios and get inspired by their work.
+          </li>
+          <li>
+            Easy to use: Project-Showroom is designed with simplicity in mind,
+            making it easy for you to create and manage your portfolio.
+          </li>
+          <li>
+            Customize your profile: Personalize your profile with your own
+            branding and make it stand out from the crowd.
+          </li>
+        </ul>
+        <b>Getting started:</b>
+        <p>
+          To get started with Project-Showroom, simply create an account and
+          start building your portfolio. You can add projects, customize your
+          profile, and explore other portfolios with ease.
+        </p>
+        <p>
+          We hope you enjoy using Project-Showroom and look forward to seeing
+          your amazing projects!
+        </p>
+      </div>
+    );
 
   return (
     <div id={id} className="p-6">
