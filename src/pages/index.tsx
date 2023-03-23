d1aeb55b0590aba23729f8dbc38a6d88085c977e
emremutlu08 <mutluemre93@gmail.com>
2023-03-23 16:00:51 +0300

    Refactor: classnames changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ const Home: NextPage = () => {
     (state: { user: { loading: boolean; user: IUserType } }) => state.user,
   );
 
-  const mainClassNames = classNames('flex relative justify-center pt-16');
   return (
     <>
       <Head>
@@ -20,7 +19,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={mainClassNames}>
+      <main className={classNames('flex relative justify-center pt-16')}>
         <MainFeature user={user} />
       </main>
     </>
