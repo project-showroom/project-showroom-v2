988570dc70f21075222151234e138733c457ff1c
emremutlu08 <mutluemre93@gmail.com>
2023-04-09 02:19:18 +0300

    New: image preview is added

--- src/features/add-project-feature/components/add-project-form/add-project-form.tsx
+++ src/features/add-project-feature/components/add-project-form/add-project-form.tsx
@@ -2,7 +2,7 @@ import { useId, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import classNames from 'classnames';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikProps } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { SubmitButton } from '../../../../components/button-components';
@@ -24,6 +24,9 @@ const AddProjectForm = (props: { className?: string }) => {
   const { className, ...rest } = props;
 
   const [tags, setTags] = useState([]);
+  const [thumbnailUrl, setThumbnailUrl] = useState<string>(
+    'https://fakeimg.pl/400x400/?text=No+Image',
+  );
 
   const dispatch = useDispatch<AppDispatch>();
 
@@ -41,7 +44,7 @@ const AddProjectForm = (props: { className?: string }) => {
 
   const initialValues: IAddProjectFormValues = {
     projectTitle: '',
-    thumbnailUrl: '',
+    thumbnailUrl: 'https://fakeimg.pl/400x400/?text=No+Image',
     description: '',
     skillTags: [],
     leftButtonTitle: 'View Online',
@@ -62,6 +65,7 @@ const AddProjectForm = (props: { className?: string }) => {
   const spaceClassNames = classNames('flex flex-col h-full p-4 mt-2');
   const leftRightButtonClassNames = classNames('gap-3 md:flex ');
   const headerLoginButtonClassNames = classNames('bg-blue-600');
+
   return (
     <div id={id} {...rest} className={classNames(className, COMPONENT_NAME)}>
       <Formik
@@ -80,97 +84,111 @@ const AddProjectForm = (props: { className?: string }) => {
           });
         }}
       >
-        <Form>
-          <FormInputText
-            id="projectTitle"
-            name="projectTitle"
-            label="Project Title"
-            placeholder="Project Title"
-            type="text"
-          />
-          <div className={spaceClassNames} />
-          <FormInputText
-            id="thumbnailUrl"
-            name="thumbnailUrl"
-            label="Thumbnail Url"
-            placeholder="Thumbnail Url"
-            type="url"
-          />
-          <div className={spaceClassNames} />
-          <FormInputText
-            id="description"
-            name="description"
-            label="Description"
-            placeholder="Description"
-            variant="outlined"
-            type="text"
-            multiline
-          />
+        {(props: FormikProps<IAddProjectFormValues>) => {
+          setThumbnailUrl(props.values.thumbnailUrl || '');
 
-          <div className={spaceClassNames} />
-          <FormAutoComplete
-            id="skillTags"
-            name="skillTags"
-            label="Skill Tags (ex: ReactJs, Materialui)"
-            helperText="If you cannot find your technology, you can write it anyway."
-            setTags={setTags}
-            allTechs={allTechs}
-            tags={tags}
-          />
-          <div className={spaceClassNames} />
-          <div className={leftRightButtonClassNames}>
-            <FormInputText
-              id="leftButtonTitle"
-              name="leftButtonTitle"
-              placeholder="View Online"
-              label="Left Button Title"
-              className="w-full"
-              type="text"
-            />
-            <FormInputText
-              id="leftButtonUrl"
-              name="leftButtonUrl"
-              helperText="(If you don't have url for this button, please
-        leave it empty)"
-              label="Left Button Url"
-              className="w-full"
-              type="url"
-            />
-          </div>
-          <div className={spaceClassNames} />
-          <div className={leftRightButtonClassNames}>
-            <FormInputText
-              id="rightButtonTitle"
-              name="rightButtonTitle"
-              placeholder="View Codes"
-              label="Right Button Title"
-              className="w-full"
-              type="text"
-            />
-            <FormInputText
-              id="rightButtonUrl"
-              name="rightButtonUrl"
-              helperText="(If you don't have url for this button, please
-        leave it empty)"
-              label="Right Button Url"
-              className="w-full"
-              type="url"
-            />
-          </div>
-          <div className={spaceClassNames} />
+          return (
+            <Form>
+              <FormInputText
+                id="projectTitle"
+                name="projectTitle"
+                label="Project Title"
+                placeholder="Project Title"
+                type="text"
+              />
+              <div className={spaceClassNames} />
+              <div className="flex justify-between items-center gap-1">
+                <FormInputText
+                  id="thumbnailUrl"
+                  name="thumbnailUrl"
+                  label="Thumbnail Url"
+                  placeholder="Thumbnail Url"
+                  type="url"
+                  className="w-5/6"
+                />
+                <img
+                  className="w-14 h-14 sm:w-40 sm:h-40"
+                  alt="Picture of the project"
+                  src={thumbnailUrl}
+                />
+              </div>
+              <div className={spaceClassNames} />
+              <FormInputText
+                id="description"
+                name="description"
+                label="Description"
+                placeholder="Description"
+                variant="outlined"
+                type="text"
+                multiline
+              />
 
-          <Box className={boxClassNames}>
-            <SubmitButton
-              className={headerLoginButtonClassNames}
-              buttonIcon={<SendIconElement />}
-              type="submit"
-              variant="contained"
-              size="large"
-            >
-              Send
-            </SubmitButton>
-          </Box>
-        </Form>
+              <div className={spaceClassNames} />
+              <FormAutoComplete
+                id="skillTags"
+                name="skillTags"
+                label="Skill Tags (ex: ReactJs, Materialui)"
+                helperText="If you cannot find your technology, you can write it anyway."
+                setTags={setTags}
+                allTechs={allTechs}
+                tags={tags}
+              />
+              <div className={spaceClassNames} />
+              <div className={leftRightButtonClassNames}>
+                <FormInputText
+                  id="leftButtonTitle"
+                  name="leftButtonTitle"
+                  placeholder="View Online"
+                  label="Left Button Title"
+                  className="w-full"
+                  type="text"
+                />
+                <FormInputText
+                  id="leftButtonUrl"
+                  name="leftButtonUrl"
+                  helperText="(If you don't have url for this button, please
+        leave it empty)"
+                  label="Left Button Url"
+                  className="w-full"
+                  type="url"
+                />
+              </div>
+              <div className={spaceClassNames} />
+              <div className={leftRightButtonClassNames}>
+                <FormInputText
+                  id="rightButtonTitle"
+                  name="rightButtonTitle"
+                  placeholder="View Codes"
+                  label="Right Button Title"
+                  className="w-full"
+                  type="text"
+                />
+                <FormInputText
+                  id="rightButtonUrl"
+                  name="rightButtonUrl"
+                  helperText="(If you don't have url for this button, please
+        leave it empty)"
+                  label="Right Button Url"
+                  className="w-full"
+                  type="url"
+                />
+              </div>
+              <div className={spaceClassNames} />
+
+              <Box className={boxClassNames}>
+                <SubmitButton
+                  className={headerLoginButtonClassNames}
+                  buttonIcon={<SendIconElement />}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                >
+                  Send
+                </SubmitButton>
+              </Box>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
--- src/features/edit-project-feature/components/edit-project-form/edit-project-form.tsx
+++ src/features/edit-project-feature/components/edit-project-form/edit-project-form.tsx
@@ -2,7 +2,7 @@ import { useId, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import classNames from 'classnames';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikProps } from 'formik';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -33,6 +33,9 @@ const EditProjectForm = (props: { className?: string }) => {
   );
 
   const [tags, setTags] = useState(editProject?.skillTags);
+  const [thumbnailUrl, setThumbnailUrl] = useState<string>(
+    'https://fakeimg.pl/400x400/?text=No+Image',
+  );
 
   const onSubmit = async (values: IAddProjectFormValues) => {
     dispatch(updateProject({ values, cardid }));
@@ -69,115 +72,129 @@ const EditProjectForm = (props: { className?: string }) => {
           onSubmit(initialValues);
         }}
       >
-        <Form>
-          <FormInputText
-            id="editProjectTitle"
-            name="editProjectTitle"
-            label="Project Title"
-            placeholder="Project Title"
-            type="text"
-            defaultValue={editProject?.projectTitle}
-            onChange={(e) => {
-              initialValues.projectTitle = e.target.value;
-            }}
-          />
-          <div className={spaceClassNames} />
-          <FormInputText
-            id="editThumbnailUrl"
-            name="editThumbnailUrl"
-            label="Thumbnail Url"
-            placeholder="Thumbnail Url"
-            type="url"
-            defaultValue={editProject?.thumbnailUrl}
-            onChange={(e) => {
-              initialValues.thumbnailUrl = e.target.value;
-            }}
-          />
-          <div className={spaceClassNames} />
-          <FormInputText
-            id="editDescription"
-            name="editDescription"
-            label="Description"
-            placeholder="Description"
-            variant="outlined"
-            type="text"
-            multiline
-            defaultValue={editProject?.description}
-            onChange={(e) => {
-              initialValues.description = e.target.value;
-            }}
-          />
+        {(props: FormikProps<IAddProjectFormValues>) => {
+          setThumbnailUrl(props.values.thumbnailUrl || '');
 
-          <div className={spaceClassNames} />
-          <FormAutoComplete
-            id="editSkillTags"
-            name="editSkillTags"
-            label="Skill Tags (ex: ReactJs, Materialui)"
-            helperText="If you cannot find your technology, you can write it anyway."
-            setTags={setTags}
-            tags={tags}
-            allTechs={allTechs}
-          />
-          <div className={spaceClassNames} />
-          <div className={leftRightButtonClassNames}>
-            <FormInputText
-              id="leftButtonTitle"
-              name="leftButtonTitle"
-              label="Left Button Title"
-              className="w-full"
-              type="text"
-            />
-            <FormInputText
-              id="editLeftButtonUrl"
-              name="editLeftButtonUrl"
-              helperText="(If you don't have url for this button, please
-        leave it empty)"
-              label="Left Button Url"
-              className="w-full"
-              type="url"
-              defaultValue={editProject?.leftButtonUrl}
-              onChange={(e) => {
-                initialValues.leftButtonUrl = e.target.value;
-              }}
-            />
-          </div>
-          <div className={spaceClassNames} />
-          <div className={leftRightButtonClassNames}>
-            <FormInputText
-              id="rightButtonTitle"
-              name="rightButtonTitle"
-              label="Right Button Title"
-              className="w-full"
-              type="text"
-            />
-            <FormInputText
-              id="editRightButtonUrl"
-              name="editRightButtonUrl"
-              helperText="(If you don't have url for this button, please
-        leave it empty)"
-              label="Right Button Url"
-              className="w-full"
-              type="url"
-              defaultValue={editProject?.rightButtonUrl}
-              onChange={(e) => {
-                initialValues.rightButtonUrl = e.target.value;
-              }}
-            />
-          </div>
-          <div className={spaceClassNames} />
+          return (
+            <Form>
+              <FormInputText
+                id="editProjectTitle"
+                name="editProjectTitle"
+                label="Project Title"
+                placeholder="Project Title"
+                type="text"
+                defaultValue={editProject?.projectTitle}
+                onChange={(e) => {
+                  initialValues.projectTitle = e.target.value;
+                }}
+              />
+              <div className={spaceClassNames} />
+              <div className="flex justify-between items-center gap-1">
+                <FormInputText
+                  id="editThumbnailUrl"
+                  name="editThumbnailUrl"
+                  label="Thumbnail Url"
+                  placeholder="Thumbnail Url"
+                  type="url"
+                  defaultValue={editProject?.thumbnailUrl}
+                  onChange={(e) => {
+                    initialValues.thumbnailUrl = e.target.value;
+                  }}
+                  className="w-5/6"
+                />
+                <img
+                  className="w-14 h-14 sm:w-40 sm:h-40"
+                  alt="Picture of the project"
+                  src={thumbnailUrl}
+                />
+              </div>
+              <div className={spaceClassNames} />
+              <FormInputText
+                id="editDescription"
+                name="editDescription"
+                label="Description"
+                placeholder="Description"
+                variant="outlined"
+                type="text"
+                multiline
+                defaultValue={editProject?.description}
+                onChange={(e) => {
+                  initialValues.description = e.target.value;
+                }}
+              />
 
-          <Box className={boxClassNames}>
-            <SubmitButton
-              className={headerLoginButtonClassNames}
-              buttonIcon={<SendIconElement />}
-              type="submit"
-              variant="contained"
-              size="large"
-            >
-              Update
-            </SubmitButton>
-          </Box>
-        </Form>
+              <div className={spaceClassNames} />
+              <FormAutoComplete
+                id="editSkillTags"
+                name="editSkillTags"
+                label="Skill Tags (ex: ReactJs, Materialui)"
+                helperText="If you cannot find your technology, you can write it anyway."
+                setTags={setTags}
+                tags={tags}
+                allTechs={allTechs}
+              />
+              <div className={spaceClassNames} />
+              <div className={leftRightButtonClassNames}>
+                <FormInputText
+                  id="leftButtonTitle"
+                  name="leftButtonTitle"
+                  label="Left Button Title"
+                  className="w-full"
+                  type="text"
+                />
+                <FormInputText
+                  id="editLeftButtonUrl"
+                  name="editLeftButtonUrl"
+                  helperText="(If you don't have url for this button, please
+        leave it empty)"
+                  label="Left Button Url"
+                  className="w-full"
+                  type="url"
+                  defaultValue={editProject?.leftButtonUrl}
+                  onChange={(e) => {
+                    initialValues.leftButtonUrl = e.target.value;
+                  }}
+                />
+              </div>
+              <div className={spaceClassNames} />
+              <div className={leftRightButtonClassNames}>
+                <FormInputText
+                  id="rightButtonTitle"
+                  name="rightButtonTitle"
+                  label="Right Button Title"
+                  className="w-full"
+                  type="text"
+                />
+                <FormInputText
+                  id="editRightButtonUrl"
+                  name="editRightButtonUrl"
+                  helperText="(If you don't have url for this button, please
+        leave it empty)"
+                  label="Right Button Url"
+                  className="w-full"
+                  type="url"
+                  defaultValue={editProject?.rightButtonUrl}
+                  onChange={(e) => {
+                    initialValues.rightButtonUrl = e.target.value;
+                  }}
+                />
+              </div>
+              <div className={spaceClassNames} />
+
+              <Box className={boxClassNames}>
+                <SubmitButton
+                  className={headerLoginButtonClassNames}
+                  buttonIcon={<SendIconElement />}
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                >
+                  Update
+                </SubmitButton>
+              </Box>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
