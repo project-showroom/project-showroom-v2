c026d8a552b7431b163dc6b231587c3ff6d8556e
emremutlu08 <mutluemre93@gmail.com>
2023-04-12 04:41:54 +0300

    New: Added react-toastify

--- src/libs/api/profile/update-profile.ts
+++ src/libs/api/profile/update-profile.ts
@@ -1,19 +1,22 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 import { IProfileType } from '../../../types/api-types';
 
 export const postAndUpdateProfile = createAsyncThunk(
   'profile/putProfile',
   async (values: IProfileType) => {
-    return await axios.put(
-      `/api/profiles`,
-      { data: values },
-      {
-        headers: {
-          'Content-Type': 'application/json',
+    return await axios
+      .put(
+        `/api/profiles`,
+        { data: values },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    );
+      )
+      .then((res) => toast(res.data.message));
   },
 );
