52e192b8f57553cb8841b75543d5aa1df1aff2ca
emremutlu08 <mutluemre93@gmail.com>
2023-04-20 17:00:04 +0300

    Removed: Delete toast

--- src/libs/api/projects/delete-project.ts
+++ src/libs/api/projects/delete-project.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 
 export const deleteProject = createAsyncThunk(
   'projects/deleteProject',
@@ -9,7 +8,6 @@ export const deleteProject = createAsyncThunk(
       .delete(`/api/projects/delete-project/${cardid}`)
       .then((response) => {
         return response.data.data;
-      })
-      .then((res) => toast(res.data.message));
+      });
   },
 );
